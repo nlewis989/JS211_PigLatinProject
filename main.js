b184aa5661e41c99dbe35f70bc0f5dea7595da0f
nlewis989 <71626506+nlewis989@users.noreply.github.com>
2021-10-04 15:33:35 -0500

    final

--- main.js
+++ main.js
@@ -10,12 +10,147 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+// put functions from class here
+
+function smallestNonNegative(num1, num2){
+  if (num1<0) {
+    return num2
+  }
+
+  if (num2<0) {
+    return num1
+  }
+
+  if (num1<num2) {
+    return num1
+  } else {
+    return num2
+  }
+
+}
+
+
+
+let indexOfFirstVowel = function(word) {
+  // first fine the indexes of all vowels
+  // compare vowels one at time, carrying the smaller indexed vowel
+  let aIndex = word.indexOf("a")
+  let eIndex = word.indexOf("e")
+  let iIndex = word.indexOf("i")
+  let oIndex = word.indexOf("o")
+  let uIndex = word.indexOf("u")
+
+  let vowelArray = [aIndex, eIndex, iIndex, oIndex, uIndex]
+
+  let vowel = -1
+
+  for (let x = 0; x < vowelArray.length; x++) {
+    vowel = smallestNonNegative(vowel, vowelArray[x])
+  }
+
+  return vowel
+}
+
+/**
+ * RULES:
+ *
+ * if the word starts with a vowel, add -yay to the end
+ * EX: egg -> eggyay; elephant -> elephantyay
+ *
+ * if the word has a vowel, then split the word in to 2 parts at the vowel
+ * then swap the 2 parts, then add -ay to the end
+ * EX: fox -> f + ox -> oxf -> oxfay
+ *     conditional -> c + onditional -> onditionalc -> onditionalcay
+ *
+ * if the word has no vowel, then add -ay to the end
+ * EX: tsk -> tskay; pftt -> pfttay
+ *
+ */
+
+/**
+ * ORDER OF OPERATIONS:
+ *
+ * *Put all words into an array*
+ * First, lowercase and remove spaces from array values
+ * Second, fine the index of the first vowel of each word. Then,
+ * - if the index = 0, add "yay" to the end of the word
+ * - if the index = -1, add "ay" to the end of the word
+ * - if the index > 0, then,
+ * -- get substring from 0 to vowelIndex, and substring from vowelIndex to end of word
+ * -- Take substrings and put end of word before beginning of word
+ * -- add "ay" to the end of the new string
+ *
+ */
+
 
 const pigLatin = (word) => {
 
-  // Your code here
+  // put all words into an array
+
+  let wordArray = word.split(" ")
+
+  wordArray = wordArray.filter(Boolean)
+
+  // Lowercase and remove spaces from array values
+  function scrubInput(str) {
+    str = str.toLowerCase();
+    str = str.replace(/ /g, '')
+    return str
+  }
+
+  for (let i = 0; i < wordArray.length; i++) {
+    wordArray[i] = scrubInput(wordArray[i])
+  }
+
+  for (let i = 0; i < wordArray.length; i++) {
+
+    // Find the index of the first vowel, store in a variable
+
+    let firstVowelIndex = indexOfFirstVowel(wordArray[i])
+
+    // if the index = 0, add "yay" to the end of the word
+
+    if (firstVowelIndex === 0) {
+      wordArray[i] = wordArray[i].concat("yay")
+    }
+
+    // if the index = -1, add "ay" to the od of the word
+
+    if (firstVowelIndex === -1) {
+      wordArray[i] = wordArray[i].concat("ay")
+    }
+
+    // if the index > 0, then,
+
+    if (firstVowelIndex > 0) {''
+
+    // -- get substring from 0 to vowelIndex, and substring from vowelIndex to end of word
+
+      let part1 = wordArray[i].substring(0, firstVowelIndex)
+      let part2 = wordArray[i].substring(firstVowelIndex)
+
+    // -- Take substrings and put end of word before beginning of word
+
+      let flippedWord = part2.concat(part1)
+
+    // -- add "ay" to the end of the new string
+
+      wordArray[i] = flippedWord.concat("ay")
+
+    }
+
+  }
+
+  // return the changed string
+
+  word = wordArray.toString()
+
+  word = word.replace(/,/g, ' ')
+
+  return word
 
 }
+
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
@@ -38,7 +173,7 @@ if (typeof describe === 'function') {
       assert.equal(pigLatin('dog'), 'ogday');
     });
     it('should translate a complex word', () => {
-      assert.equal(pigLatin('create'), 'eatecray');
+      assert.equal(pigLatin('create'), 'eatecray');npm
       assert.equal(pigLatin('valley'), 'alleyvay');
     });
     it('should attach "yay" if word begins with vowel', () => {
@@ -55,17 +190,3 @@ if (typeof describe === 'function') {
   getPrompt();
 
 }
-
-
-
-
-
-
-// **********
-//   HINTS
-// **********
-
-// break your code into pieces and focus on one piece at a time...
-// 1. if word begins with a vowel send to one function: adds "yay"
-// 2. if word begins in with a consonant send to another function: splices off beginning, returns word with new ending.
-// 3. if multiple words, create array of words, loop over them, sending them to different functions and creating a new array with the new words.
